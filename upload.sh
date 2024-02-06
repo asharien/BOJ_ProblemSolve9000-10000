7e6186fbf739fd97281f6dbcb2987b6cde7bddf0
asharien <asharien@naver.com>
2024-02-06 19:30:00 +0900

    Upload from Termux Tue Feb  6 19:30:00 KST 2024

--- upload.sh
+++ upload.sh
@@ -3,6 +3,7 @@ today=$(date)
 echo $today
 git config --global user.name asharien
 git config --global user.email asharien@naver.com 
+git remote add origin https://github.com/asharien/BOJ_ProblemSolve9000-10000.git 
 git add .
 git status
 git commit -m "Upload from Termux $today"
